a2d4bacd0611772a6b1f9c248143be28fba484d9
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2021-12-24 09:14:22 +0630

    api stockcontroller gettingstock

--- app/Http/Controllers/API/StockController.php
+++ app/Http/Controllers/API/StockController.php
@@ -10,29 +10,201 @@ class StockController extends Controller
 {
     public function gettingStock()
     {
-        $stock = Stock::latest()->first();
-        if($stock != null){
-            $selected_stock1 = substr( $stock->selected_stock, 0, 1); 
-            $selected_stock2 = substr( $stock->selected_stock, 1, 1); 
-            
-            $selected_log = SelectedLog::latest()->first();
-            $data["stock"] = $stock->stock;
+        $selected_log = SelectedLog::whereDate('date', '=', date('Y-m-d'))->first();
+        // return $selected_log;
+        if($selected_log != null) {
+            if($selected_log->evening_second_select != null){   
+                $data = $this->eveningSecondSelect($selected_log);
+            }elseif($selected_log->evening_first_select != null){
+                $data = $this->eveningFirstSelect($selected_log);
+            }
+            elseif($selected_log->morning_second_select != null){
+                $data = $this->morningSecondSelect($selected_log);
+            }else{
+                $data = $this->morningFirstSelect($selected_log);
+            }
+           
+        }else{
+            $latestStock = $this->getLatestStock();
+            // return $latestStock;
+            $data["stock"] =  $latestStock->stock;
             $data["selected_stock1"] = [
-                "selected_stock1" => $selected_stock1,
-                "stock1_stop" => $selected_stock1 == $selected_log->morning_first_select || $selected_stock1 == $selected_log->evening_first_select ? true: false,
+                "selected_stock1" => substr($latestStock->selected_stock, 0, 1),
+                "stock1_stop" => false
             ];
             $data["selected_stock2"] = [
-                "selected_stock2" => $selected_stock2,
-                "stock2_stop" => $selected_stock2 == $selected_log->morning_second_select || $selected_stock2 == $selected_log->evening_second_select ? true: false,
+                "selected_stock2" => substr($latestStock->selected_stock, 1, 1),
+                "stock2_stop" => false
             ];
-            $data["date"] = $stock->created_at->format('m/d/Y h:i:s A');
-            $data["selected_stock2"] = [
-                "selected_stock2" => $selected_stock2,
-                "stock2_stop" =>  !$data["selected_stock1"]["stock1_stop"]? false: $data["selected_stock2"]["stock2_stop"],
+            $data["is_morning"] = true;
+            // $data2["date"] = $selected_log->evening_ss_time->format('m/d/Y h:i:s A');
+            $data["date"] =  $latestStock->created_at->format('m/d/Y h:i:s A');
+            $data = [
+                $data
             ];
-            
         }
-       
         return response()->json($data);
+    }
+
+    public function getStockById($id)
+    {  
+        $stock = Stock::find($id);
+        if($stock == null){
+            return "";
+        }
+        return $stock->stock;
+    }
+
+    public function eveningSecondSelect(SelectedLog $selected_log)
+    {  
+        //data 1
+        $data1["stock"] = $this->getStockById($selected_log->mss_stock_id);
+        $data1["selected_stock1"] = [
+            "selected_stock1" => $selected_log->morning_first_select,
+            "stock1_stop" => true
+        ];
+        $data1["selected_stock2"] = [
+            "selected_stock2" => $selected_log->morning_second_select,
+            "stock2_stop" => true
+        ];
+        $data1["is_morning"] = true;
+        $data1["date"] = $selected_log->morning_ss_time;
+        // $data1["date"] = $selected_log->morning_ss_time->format('m/d/Y h:i:s A');
+
+        //data2
+        $data2["stock"] = $this->getStockById($selected_log->ess_stock_id);
+        $data2["selected_stock1"] = [
+            "selected_stock1" => $selected_log->evening_first_select,
+            "stock1_stop" => true
+        ];
+        $data2["selected_stock2"] = [
+            "selected_stock2" => $selected_log->evening_second_select,
+            "stock2_stop" => true
+        ];
+        $data2["is_morning"] = false;
+        // $data2["date"] = $selected_log->evening_ss_time->format('m/d/Y h:i:s A');
+        $data2["date"] = $selected_log->evening_ss_time;
+
+        
+        $data =[
+            $data1,
+            $data2
+        ];
+        return $data;
+    }
+
+    public function eveningFirstSelect(SelectedLog $selected_log)
+    {  
+        //data 1
+        $data1["stock"] = $this->getStockById($selected_log->mss_stock_id);
+        $data1["selected_stock1"] = [
+            "selected_stock1" => $selected_log->morning_first_select,
+            "stock1_stop" => true
+        ];
+        $data1["selected_stock2"] = [
+            "selected_stock2" => $selected_log->morning_second_select,
+            "stock2_stop" => true
+        ];
+        $data1["is_morning"] = true;
+        $data1["date"] = $selected_log->morning_ss_time;
+        // $data1["date"] = $selected_log->morning_ss_time->format('m/d/Y h:i:s A');
+
+        //data2
+        $latestStock = $this->getLatestStock();
+        $data2["stock"] =  $latestStock->stock;
+        $data2["selected_stock1"] = [
+            "selected_stock1" => $selected_log->evening_first_select,
+            "stock1_stop" => true
+        ];
+        $data2["selected_stock2"] = [
+            "selected_stock2" => substr($latestStock->selected_stock, 1, 1),
+            "stock2_stop" => false
+        ];
+        $data2["is_morning"] = false;
+        // $data2["date"] = $selected_log->evening_ss_time->format('m/d/Y h:i:s A');
+        $data2["date"] =  $latestStock->created_at->format('m/d/Y h:i:s A');
+
+        
+        $data =[
+            $data1,
+            $data2
+        ];
+        return $data;
+    }
+
+    public function getLatestStock()
+    {  
+        $stock = Stock::latest()->first();
+        if($stock == null){
+            return "";
+        }
+        return $stock;
+    }
+
+    public function morningSecondSelect(SelectedLog $selected_log)
+    {  
+        //data 1
+        $data1["stock"] = $this->getStockById($selected_log->mss_stock_id);
+        $data1["selected_stock1"] = [
+            "selected_stock1" => $selected_log->morning_first_select,
+            "stock1_stop" => true
+        ];
+        $data1["selected_stock2"] = [
+            "selected_stock2" => $selected_log->morning_second_select,
+            "stock2_stop" => true
+        ];
+        $data1["is_morning"] = true;
+        $data1["date"] = $selected_log->morning_ss_time;
+        // $data1["date"] = $selected_log->morning_ss_time->format('m/d/Y h:i:s A');
+
+        //data2
+        $latestStock = $this->getLatestStock($selected_log->mss_stock_id);
+        if($latestStock->id == $selected_log->mss_stock_id){
+            $data =[
+                $data1
+            ];
+            return $data;
+        }
+        $data2["stock"] =  $latestStock->stock;
+        $data2["selected_stock1"] = [
+            "selected_stock1" => substr($latestStock->selected_stock, 0, 1),
+            "stock1_stop" => false
+        ];
+        $data2["selected_stock2"] = [
+            "selected_stock2" => substr($latestStock->selected_stock, 1, 1),
+            "stock2_stop" => false
+        ];
+        $data2["is_morning"] = false;
+        // $data2["date"] = $selected_log->evening_ss_time->format('m/d/Y h:i:s A');
+        $data2["date"] =  $latestStock->created_at->format('m/d/Y h:i:s A');
+        $data =[
+            $data1,
+            $data2
+        ];
+        return $data;
+    }
+
+    public function morningFirstSelect(SelectedLog $selected_log)
+    {  
+        //data 1
+        $latestStock = $this->getLatestStock();
+
+        $data1["stock"] = $latestStock->stock;
+        $data1["selected_stock1"] = [
+            "selected_stock1" => $selected_log->morning_first_select,
+            "stock1_stop" => true
+        ];
+        $data1["selected_stock2"] = [
+            "selected_stock2" => substr($latestStock->selected_stock, 1, 1),
+            "stock2_stop" => false
+        ];
+        $data1["is_morning"] = true;
+        $data1["date"] =  $latestStock->created_at->format('m/d/Y h:i:s A');
+        // $data1["date"] = $selected_log->morning_ss_time->format('m/d/Y h:i:s A');
+        
+        $data =[
+            $data1
+        ];
+        return $data;
     }
 }
