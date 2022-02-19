12329552e23e64774343c80e89a90365ac554323
Lwin-Moe-Aung <lwinmoeaung.ycc@gmail.com>
2022-02-19 22:19:15 +0630

    auto

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,12 +32,12 @@ class Kernel extends ConsoleKernel
         $schedule->command('auto-generate-morning-stock')
             // ->weekdays()
             ->timezone('Asia/Yangon')
-            ->between("22:10", "22:20");
+            ->between("22:20", "22:25");
 
         $schedule->command('auto-generate-evening-stock')
             // ->weekdays()
             ->timezone('Asia/Yangon')
-            ->between("22:22", "22:30");
+            ->between("22:27", "22:30");
 
         $schedule->command('delete-live-chat')
             ->timezone('Asia/Yangon')
